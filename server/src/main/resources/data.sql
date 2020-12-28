c78b5ca59113323f9b08da067eb7d698725904d7
Frederik <freddy.white1995@gmail.com>
2020-12-28 17:37:28 +0100

    Activated the game_history testdata

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -10,15 +10,16 @@ insert into user(id, username, password)
 values (5L, 'testuser4', 'testuser4');
 insert into user(id, username, password)
 values (6L, 'testuser5', 'testuser5');
-/*
-insert into game(id, points_user1, points_user2, userid1, userid2) values (1L, 100, 500, 1L, 2L);
-insert into game(id, points_user1, points_user2, userid1, userid2) values (2L, 200, 300, 3L, 4L);
-insert into game(id, points_user1, points_user2, userid1, userid2) values (3L, 500, 400, 4L, 2L);
-insert into game(id, points_user1, points_user2, userid1, userid2) values (4L, 200, 300, 5L, 3L);
-insert into game(id, points_user1, points_user2, userid1, userid2) values (5L, 100, 400, 6L, 1L);
 
 
+insert into game_history(id, points_user1, points_user2, username1, username2)
+values (1L, 5, 0, 1L, 2L);
+insert into game_history(id, points_user1, points_user2, username1, username2)
+values (2L, 2, 3, 3L, 4L);
+insert into game_history(id, points_user1, points_user2, username1, username2)
+values (3L, 4, 1, 4L, 2L);
+insert into game_history(id, points_user1, points_user2, username1, username2)
+values (4L, 0, 5, 5L, 3L);
+insert into game_history(id, points_user1, points_user2, username1, username2)
+values (5L, 3, 2, 6L, 1L);
 
-
-
-*/
