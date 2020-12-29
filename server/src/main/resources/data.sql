f377c58a9aaefc6c286416f45be9ddaed348779a
Frederik <freddy.white1995@gmail.com>
2020-12-29 20:17:01 +0100

    Added more test data

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,13 +1,27 @@
-insert into game_history(id, points_user1, points_user2, username1, username2)
-values (1L, 5, 0, 'x', 'a');
-insert into game_history(id, points_user1, points_user2, username1, username2)
-values (2L, 2, 3, 'User', 'x');
-insert into game_history(id, points_user1, points_user2, username1, username2)
-values (3L, 4, 1, 'Test', 'Nummer 1');
-insert into game_history(id, points_user1, points_user2, username1, username2)
-values (4L, 0, 5, 'Hans', 'Max');
-insert into game_history(id, points_user1, points_user2, username1, username2)
-values (5L, 3, 2, 'snake', 'admin');
+insert into game_history(id, created_at,points_user1, points_user2, username1, username2)
+values (1L, '2020-12-29 19:07:46.857035', 5, 0, 'x', 'Hans');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (2L, '2020-12-29 19:07:56.857035', 2, 3, 'User', 'x');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (3L, '2020-12-29 19:07:36.857035', 4, 1, 'Test', 'Nummer 1');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (4L, '2020-12-29 19:07:26.857035', 0, 5, 'Hans', 'Max');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (5L, '2020-12-29 19:07:16.857035', 3, 2, 'snake', 'admin');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (6L, '2020-12-29 20:07:56.857035', 2, 3, 'User', 'x');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (7L, '2020-12-28 15:07:56.857035', 4, 2, 'x', 'Hans');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (8L, '2020-12-27 14:07:56.857035', 4, 1, 'Max', 'x');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (9L, '2020-12-26 16:07:56.857035', 2, 3, 'User', 'x');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (10L, '2020-12-25 10:07:56.857035', 3, 4, 'x', 'Test');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (11L, '2020-12-24 11:07:56.857035', 0, 5, 'snake', 'x');
+insert into game_history(id, created_at, points_user1, points_user2, username1, username2)
+values (12L, '2020-12-23 07:07:56.857035', 4, 1, 'admin', 'x');
 
 
 
