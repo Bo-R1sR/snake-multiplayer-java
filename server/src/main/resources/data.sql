f38317d96e88730813fa47b7be713e3bf507eeb8
Frederik <freddy.white1995@gmail.com>
2020-12-29 13:17:47 +0100

    Removed the users

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,26 +1,13 @@
-insert into user(id, username, password)
-values (1L, 'admin', 'admin');
-insert into user(id, username, password)
-values (2L, 'testuser1', 'testuser1');
-insert into user(id, username, password)
-values (3L, 'testuser2', 'testuser2');
-insert into user(id, username, password)
-values (4L, 'testuser3', 'testuser3');
-insert into user(id, username, password)
-values (5L, 'testuser4', 'testuser4');
-insert into user(id, username, password)
-values (6L, 'testuser5', 'testuser5');
-
 insert into game_history(id, points_user1, points_user2, username1, username2)
-values (1L, 5, 0, 'admin', 'testuser1');
+values (1L, 5, 0, 'x', 'a');
 insert into game_history(id, points_user1, points_user2, username1, username2)
-values (2L, 2, 3, 'testuser2', 'testuser3');
+values (2L, 2, 3, 'User', 'x');
 insert into game_history(id, points_user1, points_user2, username1, username2)
-values (3L, 4, 1, 'testuser3', 'testuser1');
+values (3L, 4, 1, 'Test', 'Nummer 1');
 insert into game_history(id, points_user1, points_user2, username1, username2)
-values (4L, 0, 5, 'testuser4', 'testuser2');
+values (4L, 0, 5, 'Hans', 'Max');
 insert into game_history(id, points_user1, points_user2, username1, username2)
-values (5L, 3, 2, 'testuser5', 'admin');
+values (5L, 3, 2, 'snake', 'admin');
 
 
 
