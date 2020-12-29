3bcae292ce1166206bfc220c9faa7d688a64c2d1
Frederik <freddy.white1995@gmail.com>
2020-12-29 13:22:48 +0100

    Added save method for GameHistory

--- server/src/main/java/de/snake/server/service/HistoryService.java
+++ server/src/main/java/de/snake/server/service/HistoryService.java
@@ -5,6 +5,8 @@ import de.snake.server.repository.HistoryRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,5 +19,13 @@ public class HistoryService {
 
     public List<GameHistory> getPlayedGames(String username){
         return historyRepository.findAllByUsername1OrUsername2(username, username, Sort.by(Sort.Direction.DESC, "createdAt"));
+    }
+    public void save(Integer pointsUser1, Integer pointsUser2, String username1, String username2){
+        GameHistory gameHistory = new GameHistory();
+        gameHistory.setPointsUser1(pointsUser1);
+        gameHistory.setPointsUser2(pointsUser2);
+        gameHistory.setUsername1(username1);
+        gameHistory.setUsername2(username2);
+        historyRepository.save(gameHistory);
     }
 }
